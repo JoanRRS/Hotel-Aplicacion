34176151b07f7a58d79b511f2645e4977a8eea4a
joansantana <joansantana1897@gmail.com>
2023-08-18 10:52:01 -0400

    login y registro

--- Reserva de Hotel/Hotel-Ambiente/HotelProject/hotelapp/urls.py
+++ Reserva de Hotel/Hotel-Ambiente/HotelProject/hotelapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rooms/',views.RoomListView.as_view(), name='rooms'),
     path('rooms/<int:pk>', views.RoomDetailView.as_view(), name='rooms-detail'),
+    
 ]
